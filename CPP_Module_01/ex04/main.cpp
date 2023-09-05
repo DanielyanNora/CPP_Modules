1dbdf630ecad6bfc35a036fbfd0acbbb703ac042
Vahan <vam.golden.group.j@gmail.com>
2023-09-05 20:10:11 +0400

    sonhigosg

---
+++ CPP_Module_01/ex04/main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include <fstream>
+
+std::string replace(std::ifstream &infile, std::string &str1, std::string &str2)
+{
+    std::string result;
+    std::string line;
+    std::string found;
+}
